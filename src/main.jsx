c843fd79517a98190da9487c7655411c38deeb45
maojianhui <3605828735@qq.com>
2024-10-24 14:58:17 +0800

    chore: 安装 Redux Toolkit和持久化插件

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'antd/dist/reset.css';
 import AppRouter from './routes/router.jsx';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'; // 导入 PersistGate
+import { store, persistor } from './stores/store.js'; 
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AppRouter />
-  </StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <StrictMode>
+        <AppRouter />
+      </StrictMode>,
+    </PersistGate>
+  </Provider>
 )
---
+++ src/stores/store.js
@@ -0,0 +1,15 @@
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore } from 'redux-persist';
+import tokenReducer from './tokenstore'; 
+
+// 创建 Redux store
+const store = configureStore({
+    reducer: {
+        token: tokenReducer,
+    },
+});
+
+const persistor = persistStore(store); 
+
+export { store, persistor }; 
